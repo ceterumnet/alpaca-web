8a3770f705412ae2b239ae01c4029f6d1193134c
ceterumnet <draphael@gmail.com>
2025-05-15 12:55:59 -0500

    Added build for web root config

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    allowedHosts: ['raspberrypi.local', 'localhost', '127.0.0.1'],
-    cors: {
-      origin: ['http://raspberrypi.local', 'http://localhost:32323', 'http://127.0.0.1:32323']
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: env.VITE_APP_BASE_PATH || '/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     },
-    proxy: {
-      '^/discovery/': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
+    server: {
+      allowedHosts: ['raspberrypi.local', 'localhost', '127.0.0.1'],
+      cors: {
+        origin: ['http://raspberrypi.local', 'http://localhost:32323', 'http://127.0.0.1:32323']
       },
-      '^/proxy/': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
+      proxy: {
+        '^/discovery/': {
+          target: 'http://localhost:3000',
+          changeOrigin: true
+        },
+        '^/proxy/': {
+          target: 'http://localhost:3000',
+          changeOrigin: true
+        }
       }
     }
   }
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,19 +1,22 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import viteConfigFn from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
-    },
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig((env) => {
+  const viteRunConfig = viteConfigFn(env)
+  return mergeConfig(
+    viteRunConfig,
+    defineConfig({
+      test: {
+        environment: 'jsdom',
+        exclude: [...configDefaults.exclude, 'e2e/**'],
+        root: fileURLToPath(new URL('./', import.meta.url))
+      },
+      resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
       }
-    }
-  })
-)
+    })
+  )
+})
